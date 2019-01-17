a22f4fc038ec793d4781b29c6bfdc0b6014db0ad
isakal <sakyYT@gmail.com>
2019-01-17 22:03:27 +0100

    Added login and register html file, still gotta fix flash username bug

--- website.py
+++ website.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
-app.config["SECRET_KEY"] = open("SECRET_KEY.txt","r")
+app.config['SECRET_KEY'] = '47e2d772b1ae98f3ad199dd2e1793757'
 
 posts = [
 
@@ -25,25 +24,36 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template("home.html",posts=posts,title=None)
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-	return render_template("about.html",title="About")
+    return render_template('about.html', title='About')
 
-@app.route("/register")
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-	form=RegistrationForm()
-	return render_template("registration.html",title="Register",form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {}!'.format("form.username.data"), 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
-app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-	form=LoginForm()
-	return render_template("login.html",title="Login",form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
 
-	#12:19 pt3
+#31:55
