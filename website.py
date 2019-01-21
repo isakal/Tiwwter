9a0a448522fdeeb755ac39f1c141c1b9a5684bbb
isakal <sakyYT@gmail.com>
2019-01-21 20:07:58 +0100

    Fixed an issue where it would say form.username.data instead of actual username

--- website.py
+++ website.py
@@ -36,7 +36,7 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Account created for {}!'.format("form.username.data"), 'success')
+        flash('Account created for {}!'.format(form.username.data), 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
