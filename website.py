2bc9a00d402e97c76e63d2037bbba284a7fda494
isakal <sakyYT@gmail.com>
2019-01-29 21:40:49 +0100

    Moved database models to seperate file

--- website.py
+++ website.py
@@ -1,36 +1,11 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '47e2d772b1ae98f3ad199dd2e1793757'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
-
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(20), unique=True, nullable=False)
-	email = db.Column(db.String(120), unique=True, nullable=False)
-	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-	password = db.Column(db.String(60), nullable=False)
-	posts = db.relationship('Post', backref='author', lazy=True)
-
-	def __repr__(self):
-		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
-class Post(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(100), nullable=False)
-	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	content = db.Column(db.Text, nullable=False)
-	user_id= db.Column(db.Integer, db.ForeignKey("user.id"),nullable=False)
-
-	def __repr__(self):
-		return f"User('{self.title}', '{self.date_posted}')"
-
 
 posts = [
 
@@ -84,4 +59,4 @@ def login():
 if __name__ == '__main__':
 	app.run(debug=True)
 
-#17:20
+# 17:20
