2aab01b55b0ce3b609b9de4291eb0aa4d11c1173
isakal <sakyYT@gmail.com>
2019-02-16 14:20:09 +0100

    Added simple account page and fixed some redirects

--- website/routes.py
+++ website/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request
 from website import app, db, bcrypt
 from website.forms import RegistrationForm, LoginForm
 from website.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user,login_required
 
 
 posts = [
@@ -57,7 +57,8 @@ def login():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user and bcrypt.check_password_hash(user.password, form.password.data):
 			login_user(user, remember=form.remember.data)
-			return redirect(url_for('home'))
+			next_page = request.args.get('next')
+			return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
 			flash('Login Unsuccessful. Please check username and password', 'danger')
 	return render_template('login.html', title='Login', form=form)
@@ -65,3 +66,8 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+	return render_template('account.html',title='Your account')
