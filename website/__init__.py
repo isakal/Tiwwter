68e9868f75b336fedb5a1177c81f92c8e921b4fb
isakal <sakyYT@gmail.com>
2019-02-18 21:40:13 +0100

    Preparing for adding recaptcha to register form

--- website/__init__.py
+++ website/__init__.py
@@ -7,6 +7,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '47e2d772b1ae98f3ad199dd2e1793757'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LfJTpIUAAAAAB8eRPVrytaqFACnMM87AUDqU0MG'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LfJTpIUAAAAABndeU9uz5aHtFBoZkRNK_jySbNP'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
