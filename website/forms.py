774fd8527c91655bfe6a93b33bef1ff1570fd3bb
isakal <sakyYT@gmail.com>
2019-02-19 15:14:27 +0100

    Added fully working recaptcha for registering a user (missing a margin in .css)

--- website/forms.py
+++ website/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm,RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from website.models import User
@@ -13,6 +13,7 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    recaptcha= RecaptchaField()
 
     def validate_username(self,username):
         user=User.query.filter_by(username=username.data).first()
