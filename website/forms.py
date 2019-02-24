38528f46f3c439a28162c13055a48f5993dcc8b9
isakal <sakyYT@gmail.com>
2019-02-24 20:55:32 +0100

    Cleared out database and fixed repeating emails for 2 accounts bug

--- website/forms.py
+++ website/forms.py
@@ -23,7 +23,7 @@ class RegistrationForm(FlaskForm):
             raise ValidationError("That username is already taken. Please choose another one.")
 
     def validate_email(self,email):
-        user=User.query.filter_by(email=email.data).first()
+        user=User.query.filter_by(email=email.data.lower()).first()
         if user:
             raise ValidationError("That email is already taken. Please choose another one.")
 
