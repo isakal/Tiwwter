85ade3d5a7ad648319c9053e873b429674ded205
isakal <sakyYT@gmail.com>
2019-03-07 21:21:49 +0100

    Isolated and changed recaptcha and app keys into a json

--- website/__init__.py
+++ website/__init__.py
@@ -1,18 +1,34 @@
+import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 
+def getJSON(filename):
+    with open(filename,'r') as fp:
+        return json.load(fp)
+
+
+keys = getJSON("keys.json")
+appSecretKey = keys.get("app_secret_key")
+recaptcha_keys = keys['recaptcha_keys']
+recaptchaKeyPublic = recaptcha_keys['public']
+recaptchaKeyPrivate = recaptcha_keys['private']
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '47e2d772b1ae98f3ad199dd2e1793757'
+app.config['SECRET_KEY'] = appSecretKey
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6LfJTpIUAAAAAB8eRPVrytaqFACnMM87AUDqU0MG'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6LfJTpIUAAAAABndeU9uz5aHtFBoZkRNK_jySbNP'
+app.config['RECAPTCHA_PUBLIC_KEY'] = recaptchaKeyPublic
+app.config['RECAPTCHA_PRIVATE_KEY'] = recaptchaKeyPrivate
+
 db = SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
 
 from website import routes
