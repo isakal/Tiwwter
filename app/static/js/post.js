9350f338d84aad22256e06d1546a5af9e6dc4b6a
Spinzed <davor.najev@gmail.com>
2019-03-15 19:21:52 +0100

    Stylistic changes

--- app/static/js/post.js
+++ app/static/js/post.js
@@ -1,12 +1,18 @@
 function showOptions() {
-    var selectDiv = document.getElementById("selectDiv");
-    var options = document.getElementById("options");
-    selectDiv.setAttribute("style", "display: none");
-    options.setAttribute("style", "display: flex")
-}
-function hideOptions() {
-    var selectDiv = document.getElementById("selectDiv");
-    var options = document.getElementById("options");
-    selectDiv.setAttribute("style", "display: inline-block");
-    options.setAttribute("style", "display: none")
+    const updt = document.getElementById("updt");
+    const dlt = document.getElementById("dlt");
+    updt.classList.add("trueHeight");
+    dlt.classList.add("trueHeight");
+} 
+window.onclick = function (event) {
+    const updt = document.getElementById("updt");
+    const dlt = document.getElementById("dlt");
+    if(!event.target.classList.contains("show")) {
+        if(updt.classList.contains('trueHeight')) {
+            updt.classList.remove('trueHeight');
+        }
+        if(dlt.classList.contains('trueHeight')) {
+            dlt.classList.remove('trueHeight');
+        }
+    }
 }
