4377886027b0a6c65eeebbe9758a528f085c5c7c
isakal <sakyYT@gmail.com>
2019-03-20 21:20:07 +0100

    Added  semt to mail functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import json
 from flask import Flask
+from flask_mail import Mail
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -20,12 +21,19 @@ appSecretKey = keys.get("app_secret_key")
 recaptcha_keys = keys['recaptcha_keys']
 recaptchaKeyPublic = recaptcha_keys['public']
 recaptchaKeyPrivate = recaptcha_keys['private']
+MAIL_EMAIL = keys.get("mail_email")
+MAIL_PASSWORD = keys.get("mail_password")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = appSecretKey
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['RECAPTCHA_PUBLIC_KEY'] = recaptchaKeyPublic
 app.config['RECAPTCHA_PRIVATE_KEY'] = recaptchaKeyPrivate
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = MAIL_EMAIL
+app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
 
 db = SQLAlchemy(app)
 
@@ -35,5 +43,6 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+mail = Mail(app)
 
 from app import routes
--- app/forms.py
+++ app/forms.py
@@ -43,6 +43,7 @@ class UpdateProfileForm(FlaskForm):
     submit = SubmitField('Update')
     recaptcha= RecaptchaField()
     profile_pic = FileField('Update profile picture',validators=[FileAllowed(['jpg','png'])])
+    
     def validate_username(self,username):
         if username.data != current_user.username:
             user=User.query.filter_by(username=username.data).first()
@@ -54,7 +55,30 @@ class UpdateProfileForm(FlaskForm):
             user=User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError("That email is already taken. Please choose another one.")
+
+
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):        
+    email = StringField('Email',
+    validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("There is no account with that email. You must register first.")
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+        
+
