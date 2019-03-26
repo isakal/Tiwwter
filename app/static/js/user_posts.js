f53ea736c94b7f3ffe0ffec54c0d65e4c25459fe
Spinzed <davor.najev@gmail.com>
2019-03-26 21:34:17 +0100

    Edit button now disappears when clicked and reappears when Close button is clicked

--- app/static/js/user_posts.js
+++ app/static/js/user_posts.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $("#showEdit").click(function () {
         $("#theform").slideDown("slow");
+        $("#showEdit").css('display', 'none');
     });
     $("#cancel").click(function () {
         $("#theform").slideUp("slow");
+        $("#showEdit").css('display', 'inline-block');
     });
 });
