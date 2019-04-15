63c4b71980ecc2cd518e368d45d60f586f107881
isakal <sakyYT@gmail.com>
2019-04-15 08:45:49 +0200

    Removed reCaptcha from Edit profile

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ def load_user(user_id):
 
 
 class User(db.Model,UserMixin):
+	
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
@@ -36,6 +37,7 @@ class User(db.Model,UserMixin):
 
 
 class Post(db.Model):
+
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(100), nullable=False)
 	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
