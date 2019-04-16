bb3e6e9ae26e560946c861a8ea044c9dff7f563a
isakal <sakyYT@gmail.com>
2019-04-16 17:52:37 +0200

    Some styling refinements

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -6,15 +6,15 @@ errors = Blueprint('errors',__name__)
 
 @errors.app_errorhandler(404)
 def page_not_found(error):
-    return render_template('error_page.html', err_title='Page you were looking for is not found.', err_desc='Please try again later or look elsewhere'), 404
+    return render_template('error_page.html', err_title='Page you were looking for is not found.', err_desc='Please try again later or look elsewhere.'), 404
 
 
 @errors.app_errorhandler(403)
 def access_denied(error):
-    return render_template('error_page.html', err_title='Access to this specific URL is denied', err_desc='Please check your credentials and try again.'), 403
+    return render_template('error_page.html', err_title='Access to this specific URL is denied.', err_desc='Please check your credentials and try again.'), 403
 
 
 @errors.app_errorhandler(500)
 def internal_error(error):
-    return render_template('error_page.html', err_title='Internal error', err_desc='Please try again.'), 500
+    return render_template('error_page.html', err_title='Internal error occured.', err_desc='Please try again.'), 500
 
