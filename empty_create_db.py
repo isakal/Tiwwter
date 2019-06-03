d3aef2b440fae46bc0b786d2088409f401a73653
isakal <sakyYT@gmail.com>
2019-06-03 11:15:23 +0200

    Rewrite empty/create db file

--- empty_create_db.py
+++ empty_create_db.py
@@ -3,13 +3,11 @@ from app import create_app, db
 
 #creating app to access databse without starting the server
 app = create_app()
-ctx = app.app_context()
-ctx.push()
 
-#db_manipulation goes here
-db.drop_all()
-db.create_all()
+with app.app_context():
+    """db_manipulation goes here"""
+    db.drop_all()
+    db.create_all()
 
 #closing the app
 print("Database added. Exiting the program...")
-ctx.pop()
