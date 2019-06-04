8681008a3e2f8ead485330b6db6d0e9c57a9e184
isakal <sakyYT@gmail.com>
2019-06-04 16:21:13 +0200

    Got tests to work fine and cached pip files for travis

--- app/tests/tests.py
+++ app/tests/tests.py
@@ -7,21 +7,21 @@ from app.config import TestingConfig
 
 class BasicTests(unittest.TestCase):
     def setUp(self):
-        self.app = create_app()
-        self.app.config.from_object(TestingConfig)
-        with self.app.app_context():
+        self.app = create_app() #creating an instance of an app
+        self.app.config.from_object(TestingConfig) #applying Testing Configuration
+        self.client = self.app.test_client() # instancing a test client(look at flask docs for more info)
+        with self.app.app_context(): #since app hasn't been ran, we are clearing app withing app context(look at flask docs for more info )
             db.drop_all()
             db.create_all()
-        
+
 
     def tearDown(self):
         pass
 
 
     def test_main_page(self):
-        self.app = create_app()
-        response = self.app.get('/', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/', follow_redirects=True)
+        assert response.status_code == 200
 
 
 if __name__ == '__main__':
