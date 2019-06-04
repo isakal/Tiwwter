9e9cccf2c80f245b861f17cb1bd788a64e2f03ec
isakal <sakyYT@gmail.com>
2019-06-04 16:07:24 +0200

    Syncing laptop work with pc

--- app/tests/tests.py
+++ app/tests/tests.py
@@ -1,16 +1,28 @@
 import os
 import unittest
-from app import create_app
+from app import create_app, db
+from app.config import TestingConfig
 
 
 
 class BasicTests(unittest.TestCase):
-    def setup(self):
-        app = create_app()
-        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
-        db.drop_all()
-        db.create_al()
-        print(app)
+    def setUp(self):
+        self.app = create_app()
+        self.app.config.from_object(TestingConfig)
+        with self.app.app_context():
+            db.drop_all()
+            db.create_all()
+        
+
+    def tearDown(self):
+        pass
+
+
+    def test_main_page(self):
+        self.app = create_app()
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
